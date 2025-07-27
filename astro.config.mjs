2e3a58c9a4be166fece120cb5a87fc01aded91a6
allybc1124 <124095507+allybc1124@users.noreply.github.com>
2025-07-27 11:00:44 -0700

    update?

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,11 @@
+// @ts-check
+
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
-import netlify from '@astrojs/netlify';
+
 // https://astro.build/config
 export default defineConfig({
-  adapter: netlify()
+	site: 'https://example.com',
+	integrations: [mdx(), sitemap()],
 });
